b512053305263c3fd43fad002b4fecb83f1a20ba
JusticeMcNealLLC <justin.mcnealllc@gmail.com>
2025-11-16 17:13:22 -0500

    v1

--- helloName.java
+++ helloName.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
+class nameHello{
+    public String nameCreate(Scanner sc){
+        System.out.print("What is your name?");
+        String name = sc.nextLine();
+        return name;
+    }
+
+    public void printName(String name){
+        System.out.println("Hello " + name);
+    }
+}
+
 public class helloName {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+        try(Scanner sc = new Scanner(System.in)){
+            nameHello hello = new nameHello();
+            String name =hello.nameCreate(sc);
+            hello.printName(name);
+        }
     }
 }
